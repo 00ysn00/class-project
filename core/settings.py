1a404e38c469b5ab02fb2dc925d9f6a7ee36c161
00ysn00 <yasinabdi11228@gmail.com>
2025-01-30 10:15:53 +0330

    hassan

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'root',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR/"/static"
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR/"media"
+STATICFILES_DIRS = [
+    BASE_DIR/"static"
+]
